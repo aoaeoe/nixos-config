0e531986eeacc8e55bda9342e3c39456d8992200
aoaeoe <3657alleniverson@gmail.com>
2025-09-21 15:20:35 +0800

    update readme

--- hosts/evims/default.nix
+++ hosts/evims/default.nix
@@ -4,4 +4,14 @@
   imports = [
     ./hardware-configuration.nix
   ];
+
+  # config proxy, and set global http prox, for example:
+  # export HTTP_PROXY=http://192.168.110.73:7890
+  # export HTTPS_PROXY=http://192.168.110.73:7890
+  # export ALL_PROXY=socks5://192.168.110.73:7890
+  nix = {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 }
